d97b6be42b0d1217910ef5178ad724d22c4a6d4b
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-04-09 12:00:41 -0400

    initial project

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,6 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n Shopping list:\n apple\n grape\n rice\n chicken\n salmon\n ground beef\n tortilla\n steaks\n water\n broccoli " );}
+}
