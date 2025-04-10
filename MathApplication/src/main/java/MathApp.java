be7d94bcf4d6fd1586ddf8589c39b30850e84959
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-04-10 08:37:33 -0400

    Start of MathApp

---
+++ MathApplication/src/main/java/MathApp.java
@@ -0,0 +1,10 @@
+public class MathApp {
+    public static void main(String[] args) {// Question 1:
+// declare variables here
+// then code solution
+// then use System.out.println() to display results
+// ex: System.out.println("The answer is " + answer);
+// REPEAT FOR NEXT EXERCISE
+
+    }
+}
